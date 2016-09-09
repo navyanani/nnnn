69b95f0daf5e1d9291d7a5a4085c8bda7c3cb90b
navya <navyavoosakoyala@gmail.com>
2016-09-09 18:39:49 +0530

    welcome

---
+++ home.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Homepage</title>
+</head>
+<body>
+<marquee behavior="alternate">welcome</marquee>
+<a href="login.php">login</a>
+<a href="register.php">register</a>
+</body>
+</html>
